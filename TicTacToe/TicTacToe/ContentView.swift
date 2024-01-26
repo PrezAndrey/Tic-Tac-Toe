f689ec17983f936e16abf7b43d64fafcfc2be9ac
Andrey Prez <aw06.1994@yandex.ru>
2024-01-26 03:56:26 +0300

    Computer moves fixed. Win function is created

--- TicTacToe/TicTacToe/ContentView.swift
+++ TicTacToe/TicTacToe/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
     ]
     
     @State private var moves: [Move?] = Array(repeating: nil, count: 9)
+    @State private var isGameBoardDisabled: Bool = false
     
     var body: some View {
         GeometryReader { geometry in
@@ -36,10 +37,21 @@ struct ContentView: View {
                         .onTapGesture {
                             if isSquareOccupied(in: moves, forIndex: i) {
                                 moves[i] = Move(player: .human, boardIndex: i)
+                                isGameBoardDisabled = true
+                                
+                                if checkWinCondition(for: .human, in: moves) {
+                                    print("Human wins")
+                                }
                                 
                                 DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
                                     let position = determineComputerMovePosition(in: moves)
+                                    print("Position for comp try is \(position)")
                                     moves[position] = Move(player: .computer, boardIndex: position)
+                                    isGameBoardDisabled = false
+                                    
+                                    if checkWinCondition(for: .computer, in: moves) {
+                                        print("Computer wins")
+                                    }
                                 }
                             }
                         }
@@ -47,9 +59,13 @@ struct ContentView: View {
                 }
                 Spacer()
             }
+            .disabled(isGameBoardDisabled)
             .padding()
         }
     }
+    
+    
+     // Functions
     
     func isSquareOccupied(in moves: [Move?], forIndex index: Int) -> Bool {
         return !moves.contains(where: { $0?.boardIndex == index})
@@ -57,11 +73,22 @@ struct ContentView: View {
     
     func determineComputerMovePosition(in moves: [Move?]) -> Int  {
         var position = Int.random(in: 0..<9)
-        while isSquareOccupied(in: moves, forIndex: position) {
+        while !isSquareOccupied(in: moves, forIndex: position) {
             position = Int.random(in: 0..<9)
         }
-        
         return position
+    }
+    
+    func checkWinCondition(for player: Player, in moves: [Move?]) -> Bool {
+        
+        let winPatterns: Set<Set<Int>> = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+        
+        let playerMoves = moves.compactMap { $0 }.filter { $0.player == player }
+        let playerPositions = Set(playerMoves.map { $0.boardIndex })
+        
+        for pattern in winPatterns where pattern.isSubset(of: playerPositions) { return true }
+        
+        return false
     }
 }
 
