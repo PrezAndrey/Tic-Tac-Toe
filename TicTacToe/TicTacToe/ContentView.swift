88f936adcbc98551e484d26d0e79af0acdd3a5b5
Andrey Prez <aw06.1994@yandex.ru>
2024-01-26 03:04:42 +0300

    Game field is prepared, buttons are clickable

--- TicTacToe/TicTacToe/ContentView.swift
+++ TicTacToe/TicTacToe/ContentView.swift
@@ -13,21 +13,69 @@ struct ContentView: View {
         GridItem(.flexible()),
         GridItem(.flexible())
     ]
+    
+    @State private var moves: [Move?] = Array(repeating: nil, count: 9)
+    
     var body: some View {
         GeometryReader { geometry in
             VStack {
                 Spacer()
                 LazyVGrid(columns: columns, spacing: 5) {
-                    ForEach(1..<9) { elment in
+                    ForEach(0..<9) { i in
                         ZStack {
                             Circle()
                                 .foregroundColor(.red.opacity(0.5))
                                 .frame(width: geometry.size.width/3 - 15, height: geometry.size.width/3 - 15)
+                            Image(systemName: moves[i]?.indicator ?? "")
+                                .resizable()
+                                .frame(width: 40, height: 40)
+                                .font(.headline)
+                                .foregroundColor(.white)
+                                
+                        }
+                        .onTapGesture {
+                            if isSquareOccupied(in: moves, forIndex: i) {
+                                moves[i] = Move(player: .human, boardIndex: i)
+                                
+                                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                                    let position = determineComputerMovePosition(in: moves)
+                                    moves[position] = Move(player: .computer, boardIndex: position)
+                                }
+                            }
                         }
                     }
                 }
+                Spacer()
             }
+            .padding()
         }
+    }
+    
+    func isSquareOccupied(in moves: [Move?], forIndex index: Int) -> Bool {
+        return !moves.contains(where: { $0?.boardIndex == index})
+    }
+    
+    func determineComputerMovePosition(in moves: [Move?]) -> Int  {
+        var position = Int.random(in: 0..<9)
+        while isSquareOccupied(in: moves, forIndex: position) {
+            position = Int.random(in: 0..<9)
+        }
+        
+        return position
+    }
+}
+
+
+enum Player {
+    case human, computer
+}
+
+struct Move {
+    let player: Player
+    let boardIndex: Int
+    
+    var indicator: String {
+        return player == .human ? "xmark": "circle"
     }
 }
 
